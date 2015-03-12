e97cdfd3c05b275859c24dba42733a1582dca4be
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-03-12 09:47:27 -0400

    Remove paddle from positioning

--- tasks/positioning.js
+++ tasks/positioning.js
@@ -3,14 +3,6 @@ var light = new t3.PointLight();
 light.position.set(-40, 15, -20);
 scene.add(light);
 
-var makePaddle = function () {
-  var mesh = new t3.Mesh(
-        new t3.BoxGeometry(0.55, 0.5, 0.15),
-        new t3.MeshLambertMaterial({color: 'blue'}));
-  mesh.position.set(initX, initY, initZ);
-  return mesh;
-}
-
 var cube = new t3.Mesh(
         new t3.BoxGeometry(0.5, 0.5, 0.5),
         new t3.MeshLambertMaterial({color: 'blue'}));
