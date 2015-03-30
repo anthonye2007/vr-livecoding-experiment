6e249824743035cc05d4c8d4e2722964a50cd2b6
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-03-30 12:30:27 -0400

    Add more training tasks

---
+++ tasks/positioning/trainingPos4.js
@@ -0,0 +1,17 @@
+var t3 = THREE;
+var light = new t3.PointLight();
+light.position.set(-40, 15, -20);
+scene.add(light);
+
+var cube = new t3.Mesh(
+        new t3.BoxGeometry(0.5, 0.5, 0.5),
+        new t3.MeshLambertMaterial({color: 'blue'}));
+cube.position.set(2.19, 0.90, 1.49);
+cube.name = 'cube';
+scene.add(cube);
+var target = new t3.Mesh(
+        new t3.BoxGeometry(0.5, 0.5, 0.5),
+        new t3.MeshLambertMaterial({color: 'red'}));
+target.name = 'target';
+target.position.set(-15.83, 2.43, 3.14);
+scene.add(target);
