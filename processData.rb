1c3518d24b38c7a7323fd76c888e38db913e0621
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-04-13 10:33:54 -0400

    Get done time

--- processData.rb
+++ processData.rb
@@ -15,18 +15,19 @@ end
 puts done
 timestamp = done.split(']')[0] # Returns timestamp but with a leading square bracket
 timestamp.slice!(0) # Remove leading bracket, now have date, then time
-time = timestamp.split()[1] # Get time without date
-
-timeDetails = time.split(':')
-hours = timeDetails[0]
-minutes = timeDetails[1]
-seconds = timeDetails[2]
-
-puts "hours: " + hours
-puts "minutes: " + minutes
-puts "seconds: " + seconds
 
 timezoneOffset = '-05' # Eastern Time Zone is UTC-5
 parsed = DateTime.parse(timestamp + timezoneOffset)
 puts parsed
-puts parsed.to_time.to_i
+puts parsed.to_time.to_i #seconds since epoch
+
+doneTime = parsed.to_time.to_i
+
+####
+
+start = ''
+File.readlines(filePath).each do |line|
+  start = line if line.include?('Start')
+end
+
+puts start
