e1331676704c8ab4c7072294ef972596fe12a8d4
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-04-14 11:46:35 -0400

    Compute average for given env

--- processData.rb
+++ processData.rb
@@ -1,11 +1,8 @@
 require "date"
 
 #folderPath = "C:\\Users\\Anthony\\Documents\\code\\data\\participant-2\\"
-folderPath = ARGV[0]
-ext = '.log'
-filename = 'live'
-FilePath = folderPath + filename + ext
-puts FilePath
+FolderPath = ARGV[0]
+Ext = '.log'
 
 def parseTime(str)
   timestamp = str.split(']')[0] # Returns timestamp but with a leading square bracket
@@ -16,38 +13,52 @@ def parseTime(str)
   return parsed.to_time.to_i
 end
 
-def findStartTime
+def findStartTime(filename)
+  path = FolderPath + filename + Ext
   start = ''
-  File.readlines(FilePath).each do |line|
+  File.readlines(path).each do |line|
     line.strip!
     start = line if line.end_with?(':')
   end
 
   startTime = parseTime(start)
-  puts "Started at: " + startTime.to_s
+  #puts "Started at: " + startTime.to_s
 
   return startTime
 end
 
-def findEndTime
+def findEndTime(filename)
   done = ''
-
-  File.readlines(FilePath).each do |line|
+  path = FolderPath + filename + Ext
+  File.readlines(path).each do |line|
     done = line if line.include?('Done')
   end
 
   doneTime = parseTime(done)
-  puts "Finished at: " + doneTime.to_s
+  #puts "Finished at: " + doneTime.to_s
 
   return doneTime
 end
 
-def processFile
-  start = findStartTime()
-  done = findEndTime()
+def processFile(filename)
+  puts "Environment: " + filename
+  start = findStartTime(filename)
+  done = findEndTime(filename)
 
   duration = done - start
   puts "Duration (in seconds): " + duration.to_s
+  puts ''
+
+  return duration
 end
 
-processFile
+environment = 'fullHandMovement'
+
+seconds = 0
+
+seconds += processFile(environment)
+seconds += processFile(environment + '(1)')
+seconds += processFile(environment + '(2)')
+
+average = seconds / 3.0
+puts "Average time: " + average.to_s
